5beb7c6faf4405867c3dd81344f4dcb342f0ba81
Win <Win7yes@users.noreply.github.com>
2019-01-21 20:06:50 -0300

    got the interior door to work

--- lua/entities/gmod_tardis_interior/modules/sh_winmodel3.lua
+++ lua/entities/gmod_tardis_interior/modules/sh_winmodel3.lua
@@ -1,13 +1,11 @@
 if SERVER then
 
-    ENT:AddHook("Initialize","WIN_Model-3",function(self)
+    ENT:AddHook("PostPlayerInitialize","WIN_Model-3",function(self)
         if self.metadata.ID == "win-model3" then
             local intdoor = TARDIS:GetPart(self,"door")
-            timer.Simple(5, function()
-                if IsValid(intdoor) then
-                    intdoor:SetBodygroup(5, 1)
-                end
-            end)
+            if IsValid(intdoor) then
+                intdoor:SetBodygroup(5, 1)
+            end
         end
     end)
 
